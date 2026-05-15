db8467f114d93223e281fb80d282f73675c5fb85
peregrinecooling <perry.cooling@gmail.com>
2026-05-16 11:17:58 +1200

    Added Driver sensor for simulator use

--- aebs/src/main/java/swen326/group4/Main.java
+++ aebs/src/main/java/swen326/group4/Main.java
@@ -4,6 +4,8 @@ import swen326.group4.Sensors.Camera.Camera;
 import swen326.group4.Sensors.Radar_Lidar.Lidar;
 import swen326.group4.Sensors.Radar_Lidar.Radar;
 
+import swen326.group4.Sensors.Driver.Driver;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         new Camera("1", "aebs/src/test/java/Simulator/Scenarios").start();
@@ -17,5 +19,7 @@ public class Main {
         new Radar("1", "aebs/src/test/java/Simulator/Scenarios").start();
         new Radar("2", "aebs/src/test/java/Simulator/Scenarios").start();
         new Radar("3", "aebs/src/test/java/Simulator/Scenarios").start();
+
+        new Driver("aebs/src/test/java/Simulator/Scenarios").start();
     }
 }
