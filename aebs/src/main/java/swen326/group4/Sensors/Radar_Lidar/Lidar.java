81894852a247a08910c1ccbeaefea3011384c809
Frank Quigley <quiglefran@ecs.vuw.ac.nz>
2026-05-15 17:02:54 +1200

    Most basic form of Radar/LiDAR sensors

---
+++ aebs/src/main/java/swen326/group4/Sensors/Radar_Lidar/Lidar.java
@@ -0,0 +1,28 @@
+package swen326.group4.Sensors.Radar_Lidar;
+
+import java.util.ArrayList;
+
+public class Lidar{
+    private enum SensorStatus{
+        OK,
+        DEGRADED,
+        FAILED,
+    }
+    private SensorStatus status; 
+    private ArrayList<RadarLidarReading> previousReading;
+
+    private static final int UPDATE_INTERVAL_MS = 100;
+
+    // Called by simulator every 100ms
+    public void update(ArrayList<RadarLidarReading> reading) {
+        previousReading = reading;
+      }
+
+    // Called by 2oo3 voter
+    public ArrayList<RadarLidarReading> getLatestReading() {  
+        return previousReading;
+    }
+
+    public SensorStatus getStatus() { return status; }
+    public int getFrequency() { return UPDATE_INTERVAL_MS; }
+}
---
+++ aebs/src/main/java/swen326/group4/Sensors/Radar_Lidar/RadarLidarData.java
@@ -0,0 +1,8 @@
+package swen326.group4.Sensors.Radar_Lidar;
+
+record RadarLidarReading(
+    float distanceMetres,
+    float relativeSpeedKmh,
+    float bearingDegrees
+) {}
+
