6499b47ae52fea1149356f2eb2f70fa21f8b111f
Tom <marstetom@ecs.vuw.ac.nz>
2026-05-16 11:34:20 +1200

    WheelSensor, Data and Voter all implemented in initial implementation commit for integration.

--- aebs/src/main/java/swen326/group4/Main.java
+++ aebs/src/main/java/swen326/group4/Main.java
@@ -1,7 +1,16 @@
 package swen326.group4;
 
+import java.io.IOException;
+
+import swen326.group4.Sensors.Wheel_Sensor.WheelSensor;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(final String[] args) {
+        try {
+            new WheelSensor(1, "aebs/src/main/java/swen326/group4/worldWheelSpeedId1.json").start();
+        } catch (IOException e) {
+            System.err.println("Failed to open world file: " + e.getMessage());
+        }
     }
 }
