4abc380164b08cb692e1ab14be99fdd986e9f32f
peregrinecooling <perry.cooling@gmail.com>
2026-05-16 11:17:58 +1200

    Added Driver sensor for simulator use

--- aebs/src/main/java/swen326/group4/Main.java
+++ aebs/src/main/java/swen326/group4/Main.java
@@ -1,7 +1,9 @@
 package swen326.group4;
 
+import swen326.group4.Sensors.Driver.Driver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Driver("aebs/src/test/java/Simulator/Scenarios").start();
     }
 }
