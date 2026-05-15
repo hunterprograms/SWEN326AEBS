1388eeb7f47fa38cd883dc35bb9040b500574dfb
Hunter Fromont <hunter.fromont@hotmail.com>
2026-05-15 16:53:39 +1200

    DID MVC and Listener Initailization and DID Model Implementation

--- aebs/src/main/java/swen326/group4/Car/Interface.java
+++ aebs/src/main/java/swen326/group4/Car/Interface.java
@@ -1,5 +1,31 @@
 package swen326.group4.Car;
 
-public class Interface{
-    
+import javax.swing.*;
+import java.awt.Color;
+
+/**
+ * 
+ */
+public class Interface
+{
+    public static void main(String[] args){
+
+        
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.gray);
+        panel.setBounds(0, 0, 500, 250);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(1000, 500);
+        frame.setVisible(true);
+        frame.add(panel);
+
+    }
+
+
+
+
+
 }
